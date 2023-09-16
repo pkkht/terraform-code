f0e034aee22acbf0928d529c7806aec1fc625d60
pkkht <pkkht@users.noreply.github.com>
2023-09-16 17:32:24 +1000

    Updated main.tf to connect to EC2 instance via HTTP curl

--- main.tf
+++ main.tf
@@ -5,16 +5,16 @@ provider "aws" {
 variable "server_port"{
   type = number
   description = "the port for HTTP"
-  default = 8080
+  default = 80
 }
 resource "aws_instance" "name" {
-  ami = "ami-0672b175139a0f8f4"
+  ami = "ami-0310483fb2b488153"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p {var.server_port} &
+              sudo busybox httpd -f -p 80 &
               EOF
   
   user_data_replace_on_change = true
@@ -26,12 +26,31 @@ resource "aws_instance" "name" {
 
 resource "aws_security_group" "instance"{
   name="terraform-example-security-instance"
-  ingress  {
-    from_port = var.server_port
-    to_port = var.server_port
+  ingress {
+    from_port = 443
+    to_port = 443
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   tags={
     Name="terraform-example"
   }
