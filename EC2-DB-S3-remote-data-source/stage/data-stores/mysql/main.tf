79dac9e5272a125c1eece3226c7d8960272f11b6
pkkht <pkkht@users.noreply.github.com>
2023-09-18 22:12:44 +1000

    More Terraforming in AWS

---
+++ EC2-DB-S3-remote-data-source/stage/data-stores/mysql/main.tf
@@ -0,0 +1,25 @@
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+
+  # How should we set the username and password?
+  username = var.db_username
+  password = var.db_password
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-example-hariharan"
+    key            = "stage/data-stores/mysql/terraform.tfstate"
+    region         = "ap-southeast-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
---
+++ EC2-DB-S3-remote-data-source/stage/services/webserver-cluster/variables.tf
@@ -0,0 +1,5 @@
+variable "server_port"{
+  type = number
+  description = "the port for HTTP"
+  default = 8083
+}
